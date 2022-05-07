024a411a4c255da8a58971d991057f847b27135a
Oleg <reffum@bk.ru>
2022-05-07 20:51:43 +0300

    Change play model.
    Use MediaSession, MediaBrowersService etc. In process.

--- app/src/main/java/com/reffum/myyoutube/view/SearchRecycleViewAdapter.java
+++ app/src/main/java/com/reffum/myyoutube/view/SearchRecycleViewAdapter.java
@@ -1,4 +1,4 @@
-package com.reffum.myyoutube;
+package com.reffum.myyoutube.view;
 
 import android.graphics.Bitmap;
 import android.view.LayoutInflater;
@@ -9,6 +9,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.reffum.myyoutube.R;
+import com.reffum.myyoutube.VideoData;
 
 import java.util.ArrayList;
 
